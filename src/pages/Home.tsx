e28da8f3fac6bfcdecfb668ea920b67a117b9ea3
Guilherme Amado <guiii@live.com>
2021-06-21 23:35:21 -0300

    add firebase auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useHistory } from 'react-router-dom';
 
+import { auth, firebase } from '../services/firebase';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -8,6 +9,14 @@ import Button from '../components/Button';
 
 const Home = () => {
   const history = useHistory();
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      history.push('/rooms/new');
+    });
+  }
 
   return (
     <div id="page-auth">
@@ -22,10 +31,7 @@ const Home = () => {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button
-            className="create-room"
-            onClick={() => history.push('/rooms/new')}
-          >
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do Google" /> Crie sua sala com o
             Google
           </button>
