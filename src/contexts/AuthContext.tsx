f5d8284513ad291435de72555a3b14771a5829d5
Guilherme Amado <guiii@live.com>
2021-06-22 00:32:35 -0300

    add auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,73 @@
+import { createContext, useState, ReactNode, useEffect } from 'react';
+
+import { firebase, auth } from '../services/firebase';
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+};
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextType);
+
+const AuthContextProvider = (props: AuthContextProviderProps) => {
+  const [user, setUser] = useState<User>();
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error('Missing information from Google Account.');
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      });
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.');
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,21 @@
 import { useHistory } from 'react-router-dom';
 
-import { auth, firebase } from '../services/firebase';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import Button from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 const Home = () => {
+  const { user, signInWithGoogle } = useAuth();
   const history = useHistory();
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result);
-      history.push('/rooms/new');
-    });
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new');
   }
 
   return (
