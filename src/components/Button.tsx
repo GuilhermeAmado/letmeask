502949c90944fdea406c2cf94783e6e5a83aecc5
Guilherme Amado <guiii@live.com>
2021-06-21 23:12:19 -0300

    add Home page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,14 @@
-type ButtonProps = {
-  children?: string;
-};
+import { ButtonHTMLAttributes } from 'react';
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.children}</button>;
+  return (
+    <button className="button" {...props}>
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
