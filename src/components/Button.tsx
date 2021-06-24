c253b7e91fb872a1f35c6d5e858e27510f9f6105
Guilherme Amado <guiii@live.com>
2021-06-23 22:12:51 -0300

    update - add button outline prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
 import { ButtonHTMLAttributes } from 'react';
 import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  outline?: boolean;
+};
 
-const Button = (props: ButtonProps) => {
+const Button = ({ outline = false, ...props }: ButtonProps) => {
   return (
-    <button className="button" {...props}>
+    <button className={`button ${outline ? 'outlined' : ''}`} {...props}>
       {props.children}
     </button>
   );
